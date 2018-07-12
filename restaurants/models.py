a80bf2d564116a19b10b6ca2fe004356d072f4e1
mohd <meshemali@gmail.com>
2018-07-12 13:43:58 +0300

    task 06

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
+    opening_time = models.TimeField(auto_now_add=True)
+    closing_time = models.TimeField(auto_now_add=True)
